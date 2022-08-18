598b8db3a77bf68b972472aaaf5eb19195acf463
jr-cast <castanos@pm.me>
2022-08-18 12:58:10 -0500

    Fix post_counter updater function

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
   scope :user, -> { joins(:users).where(user: { user_id: id })}
 
-  def update_user_counter
-    user.update(post_counter: Post.count)
+  def update_counter
+    user.update(post_counter: Post.where(user_id: user.id).count)    
   end
 end

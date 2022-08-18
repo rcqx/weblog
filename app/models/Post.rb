86bfcc1a9030ad2e1b482a2bb775a77bb9c52e4b
jr-cast <castanos@pm.me>
2022-08-18 12:21:21 -0500

    Update posts counter for user

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :commments
   has_many :likes
+  scope :user, -> { joins(:users).where(user: { user_id: id })}
+
+  def update_user_counter
+    user.update(post_counter: Post.count)
+  end
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :commments
   has_many :likes
-  scope :posts, -> { joins(:posts).where(posts: { user_id: id })}
+  scope :posts, -> { joins(:posts).where(posts: { user_id: id }) }
+  
+  def last_posts
+    posts.limit(3)
+  end
 end

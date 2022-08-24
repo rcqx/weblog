19b6e640de60e54ed91c3a999607ff66c211ba40
jr-cast <castanos@pm.me>
2022-08-23 19:58:20 -0500

    Add request and feature specs for user_controller routes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users_controller', type: :request do
+RSpec.describe 'Users_controller_requests', type: :request do
   describe 'GET #index' do
     before(:example) { get users_path }
     
@@ -10,6 +10,18 @@ RSpec.describe 'Users_controller', type: :request do
 
     it 'renders index template correctly' do 
       expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get "/users/4245" }
+
+    it 'is receiving an Ok response from the server' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders index template correctly' do 
+      expect(response).to render_template('show')
     end
   end
 end

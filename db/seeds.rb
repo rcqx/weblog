87d991a34860da3b970a099a42055bc66c2039aa
jr-cast <castanos@pm.me>
2022-08-30 01:00:26 -0500

    Add working test using db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(name: 'John Doe', photo: 'A url', bio: 'A full stack web developer');
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Users_controller_features', js: true, type: :system do
-  describe 'GET #index features' do
+RSpec.describe 'User testing', js: true, type: :system do
+  subject!(:author) { User.first }
+
+  before(:all) do
+    Rails.application.load_seed
+  end
+
+  describe 'GET #index' do
+    before(:example) do
+      visit "users/1"
+    end
+
     it 'display specific user name in the screen' do
-      visit ('/users/1')
-      sleep(3)
       expect(page).to have_text('John Doe')
     end
   end

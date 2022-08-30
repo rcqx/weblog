813f2bf897895f06cce580e3cb6215993451eea9
jr-cast <castanos@pm.me>
2022-08-30 13:04:06 -0500

    Add index page system integration tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create(name: 'John Doe', photo: 'A url', bio: 'A full stack web developer');
+user1 = User.create(name: 'John Doe', photo: 'A url', bio: 'A full stack web developer');
+user2 = User.create(name: 'Jose Ramon', photo: 'A url', bio: 'A front-end web developer');
+user3 = User.create(name: 'Jane Doe', photo: 'A url', bio: 'A back-end web developer');
+
+
+post1 = Post.create(title: 'Ruby on Rails', text: 'I started leaningn RoR recently', user_id: user1.id);
+post2 = Post.create(title: 'chromDriver?', text: 'I started making bread', user_id: user1.id);
+post3 = Post.create(title: 'Capybara sucks', text: 'lmao', user_id: user2.id);
+post4 = Post.create(title: 'I know', text: 'roflmao', user_id: user3.id);
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,17 +3,37 @@ require 'rails_helper'
 RSpec.describe 'User testing', js: true, type: :system do
   subject!(:author) { User.first }
 
-  before(:all) do
+  before(:each) do
     Rails.application.load_seed
   end
 
-  describe 'GET #index' do
+  describe 'User index page stories test' do
     before(:example) do
-      visit "users/1"
+      visit "users"
     end
 
-    it 'display specific user name in the screen' do
+    it 'display user names on the screen' do
+      expect(page).to have_text('John Doe')
+      expect(page).to have_text('Jose Ramon')
+      expect(page).to have_text('Jane Doe')
+    end
+
+    it 'checks user images exist' do
+      imgs = page.all("img[src='https://www.example.com/image']")
+      expect(imgs.length).to eq(3)
+    end
+
+
+    it 'can see the number of posts each user has written' do
+      expect(page).to have_text('Number of posts: 2')
+      expect(page).to have_text('Number of posts: 1')
+      expect(page).to have_text('Number of posts: 1')
+    end
+
+    it 'redirects to users show page when clicking' do
+      first(".postCont").click_link("John Doe")
       expect(page).to have_text('John Doe')
     end
+
   end
 end

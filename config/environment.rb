af38ae483052933fcede8200e6171a2a689c4292
basem909 <basemshams30@gmail.com>
2022-08-31 17:23:28 -0300

    Confirm the email to sign in

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 465,
+    user_name:ENV['basemshams30@gmail.com'],
+    password: ENV[''],
+    domain: '@gmail.com',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
